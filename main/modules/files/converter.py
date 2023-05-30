6f0fe9def14b7153cbc608ebfdf8587a0f2193aa
Anirudh <anirudh.agarwal@ril.com>
2023-05-30 11:26:38 +0530

    Added Conversion utility

---
+++ main/modules/files/converter.py
@@ -0,0 +1,75 @@
+import json
+import csv
+import yaml
+from yaml import SafeLoader
+
+
+class FileConversion():
+    def __init__(self, source_ext, destination_ext, source_file_path):
+        self.source_ext = source_ext
+        self.destination_ext = destination_ext
+        self.source_file_path = source_file_path
+        return self.converter(self.source_ext, self.destination_ext, self.source_file_path)
+
+
+    def converter(self, source_ext, destination_ext, source_file_path):
+        destination_file_path = source_file_path.replace(source_ext, destination_ext)
+        if source_ext == "json" and destination_ext == "csv":
+            return self.json_to_csv(source_file_path, destination_file_path)
+            
+        elif source_ext == "csv" and destination_ext == "json":
+            return self.csv_to_json(source_file_path, destination_file_path)
+        
+        elif source_ext == "json" and destination_ext == "yaml":
+            return self.json_to_yaml(source_file_path, destination_file_path)
+        
+        elif source_ext == "yaml" and destination_ext == "json":
+            return self.yaml_to_json(source_file_path, destination_file_path)
+
+
+    def json_to_csv(self, source_file_path, destination_file_path):
+        with open(source_file_path) as json_file:
+            jsondata = json.load(json_file)
+        data_file = open(destination_file_path, 'w', newline='')
+        csv_writer = csv.writer(data_file)
+        count = 0
+        for data in jsondata:
+            if count == 0:
+                header = data.keys()
+                csv_writer.writerow(header)
+                count += 1
+            csv_writer.writerow(data.values())
+        data_file.close()
+        return destination_file_path
+
+
+    def csv_to_json(self, csvFilePath, jsonFilePath):
+        data = {}
+        with open(csvFilePath, encoding='utf-8') as csvf:
+            csvReader = csv.DictReader(csvf)
+            for rows in csvReader:
+                key = rows['No']
+                data[key] = rows
+        with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+            jsonf.write(json.dumps(data, indent=4))
+        return jsonFilePath
+
+
+    def json_to_yaml(self, source_file_path, destination_file_path):
+        with open(source_file_path) as json_file:
+            json_data = json.load(json_file)
+            for data in json_data:
+                file=open(destination_file_path,"a")
+                yaml.dump(data,file)
+            file.close()
+            return destination_file_path
+
+
+    def yaml_to_json(self, source_file_path, destination_file_path):
+        with open(source_file_path) as yaml_file:
+            python_dict=yaml.load(yaml_file, Loader=SafeLoader)
+        file=open(destination_file_path,"w")
+        json.dump(python_dict,file)
+        file.close()
+        return destination_file_path
+
