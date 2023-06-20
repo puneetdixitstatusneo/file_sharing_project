723ff6ad9a757f02013dc349bf69c4c8d9c3e13e
Anirudh <anirudh.agarwal@ril.com>
2023-06-20 15:43:11 +0530

    Added entity already exists exception

--- main/custom_exceptions/__init__.py
+++ main/custom_exceptions/__init__.py
@@ -2,10 +2,12 @@ from main.custom_exceptions.exception_handlers import (
     handle_entity_not_found_error,
     handle_unauthorized_user_error,
     handle_validation_error,
+    handle_entity_already_exists_error
 )
 from main.custom_exceptions.exceptions import (
     CustomValidationError,
     EntityNotFoundError,
+    EntityAlreadyExistsError,
     UnauthorizedUserError
 )
 
@@ -14,5 +16,6 @@ from main.custom_exceptions.exceptions import (
 CUSTOM_EXCEPTIONS = [
     (CustomValidationError, handle_validation_error),
     (UnauthorizedUserError, handle_unauthorized_user_error),
-    (EntityNotFoundError, handle_entity_not_found_error)
+    (EntityNotFoundError, handle_entity_not_found_error),
+    (EntityAlreadyExistsError, handle_entity_already_exists_error)
 ]
