92da6afdf3acf824a5a6cc6091766a2936d7c461
Anirudh <anirudh.agarwal@ril.com>
2023-07-05 02:26:03 +0530

    Refactoring

--- main/modules/files/schema_validator.py
+++ main/modules/files/schema_validator.py
@@ -6,32 +6,16 @@ class AddFileSchema(Schema):
     """
     Schema to add file to the database.
     """
-
     file_name = fields.String(required=True)
-    # file_location = fields.String(required=True)
     project_id = fields.Integer(required=True)
-
-
-class GetFileSchema(Schema):
-    """
-    Schema to get files from a project.
-    """
-
-    # file_name = fields.String(required=True)
-    # file_location = fields.String(required=True)
-    project_id = fields.Integer(required=True)
-    
 
 
 class UpdateFileSchema(Schema):
     """
     Schema to update the file.
     """
-
     file_name = fields.String(required=False)
-    # file_location = fields.String(required=False)
     project_id = fields.Integer(required=False)
-
 
 
 class FileConversionSchema(Schema):
@@ -42,4 +26,3 @@ class FileConversionSchema(Schema):
     output_file_name = fields.String(required=True)
     from_ext = fields.String(required=True)
     to_ext = fields.String(required=True)
-    project_id = fields.Integer(required=True)
